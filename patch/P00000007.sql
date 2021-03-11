8feaa98e40844d87096608287171628187bf5bf4
Преподобный Ален <alienufo@inbox.ru>
2021-03-11 10:33:04 +0300

    Patch: #7

---
+++ patch/P00000007.sql
@@ -0,0 +1,17 @@
+\ir '../entity/object/reference/version/create.psql'
+\ir '../entity/object/reference/project/create.psql'
+
+--------------------------------------------------------------------------------
+
+\connect :dbname admin
+
+SELECT SignIn(CreateSystemOAuth2(), 'admin', 'admin');
+
+SELECT GetErrorMessage();
+
+SELECT CreateEntityVersion(GetClass('reference'));
+SELECT CreateEntityProject(GetClass('reference'));
+
+SELECT SignOut();
+
+\connect :dbname kernel
