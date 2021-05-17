72eb71adf96966c581cd6187562f405c9b7c3c51
Преподобный Ален <alienufo@inbox.ru>
2021-05-17 12:34:30 +0300

    Enabled patch #14.

--- patch/P00000014.sql
+++ patch/P00000014.sql
@@ -65,3 +65,144 @@ END;
 $$ LANGUAGE plpgsql
    SECURITY DEFINER
    SET search_path = kernel, pg_temp;
+
+CREATE OR REPLACE FUNCTION db.ft_session_before()
+RETURNS TRIGGER
+AS $$
+DECLARE
+  vAgent    text;
+BEGIN
+  IF (TG_OP = 'DELETE') THEN
+    DELETE FROM db.listener WHERE session = OLD.code;
+    RETURN OLD;
+  ELSIF (TG_OP = 'UPDATE') THEN
+    IF OLD.code <> NEW.code THEN
+      RAISE DEBUG 'Hacking alert: code (% <> %).', OLD.code, NEW.code;
+      RETURN NULL;
+    END IF;
+
+    IF OLD.secret <> NEW.secret THEN
+      RAISE DEBUG 'Hacking alert: secret (% <> %).', OLD.secret, NEW.secret;
+      RETURN NULL;
+    END IF;
+
+    IF OLD.pwkey <> NEW.pwkey THEN
+      RAISE DEBUG 'Hacking alert: pwkey (% <> %).', OLD.pwkey, NEW.pwkey;
+      RETURN NULL;
+    END IF;
+
+    IF OLD.suid <> NEW.suid THEN
+      RAISE DEBUG 'Hacking alert: suid (% <> %).', OLD.suid, NEW.suid;
+      RETURN NULL;
+    END IF;
+
+    IF OLD.created <> NEW.created THEN
+      RAISE DEBUG 'Hacking alert: created (% <> %).', OLD.created, NEW.created;
+      RETURN NULL;
+    END IF;
+
+    IF NEW.salt IS NULL THEN
+      NEW.salt := OLD.salt;
+    END IF;
+
+    IF (NEW.updated - OLD.updated) > INTERVAL '1 hour' THEN
+      NEW.salt := gen_salt('md5');
+    END IF;
+
+    IF NEW.salt IS DISTINCT FROM OLD.salt THEN
+      NEW.token := NewTokenCode(NEW.oauth2, NEW.code, NEW.salt, NEW.agent, NEW.host, NEW.updated);
+    END IF;
+
+    IF NEW.area IS DISTINCT FROM OLD.area THEN
+      IF NOT IsMemberArea(NEW.area, NEW.userid) THEN
+        NEW.area := OLD.area;
+      END IF;
+    END IF;
+
+    IF OLD.interface IS DISTINCT FROM NEW.interface THEN
+      IF NOT IsMemberInterface(NEW.interface, NEW.userid) THEN
+        NEW.interface := OLD.interface;
+      END IF;
+    END IF;
+
+    RETURN NEW;
+  ELSIF (TG_OP = 'INSERT') THEN
+    IF NEW.suid IS NULL THEN
+      NEW.suid := NEW.userid;
+    END IF;
+
+    IF NEW.secret IS NULL THEN
+      NEW.secret := GenSecretKey();
+    END IF;
+
+    IF NEW.agent IS NULL THEN
+      SELECT application_name INTO vAgent FROM pg_stat_activity WHERE pid = pg_backend_pid();
+      NEW.agent := coalesce(vAgent, current_database());
+    END IF;
+
+    NEW.salt := gen_salt('md5');
+
+    IF NEW.pwkey IS NULL THEN
+      NEW.pwkey := crypt(StrPwKey(NEW.suid, NEW.secret, NEW.created), NEW.salt);
+    END IF;
+
+    NEW.code := SessionKey(NEW.pwkey, GetSecretKey());
+
+    IF NEW.token IS NULL THEN
+      NEW.token := NewTokenCode(NEW.oauth2, NEW.code, NEW.salt, NEW.agent, NEW.host, NEW.updated);
+    END IF;
+
+    IF NEW.locale IS NULL THEN
+      SELECT id INTO NEW.locale FROM db.locale WHERE id = GetDefaultLocale(NEW.userid);
+    END IF;
+
+    IF NEW.area IS NULL THEN
+      NEW.area := GetDefaultArea(NEW.userid);
+    END IF;
+
+	SELECT id INTO NEW.area FROM db.area WHERE id = NEW.area AND scope IN (SELECT GetOAuth2Scopes(NEW.oauth2));
+	IF NOT FOUND THEN
+	  SELECT id INTO NEW.area FROM db.area WHERE scope IN (SELECT GetOAuth2Scopes(NEW.oauth2)) AND type = '00000000-0000-4002-a001-000000000001'; -- main
+	END IF;
+
+    IF NOT IsMemberArea(NEW.area, NEW.userid) THEN
+      SELECT '00000000-0000-4003-a000-000000000002' INTO NEW.area; -- guest
+    END IF;
+
+    IF NEW.interface IS NULL THEN
+      NEW.interface := GetDefaultInterface(NEW.userid);
+    END IF;
+
+    IF NOT IsMemberInterface(NEW.interface, NEW.userid) THEN
+      SELECT '00000000-0000-4004-a000-000000000003' INTO NEW.interface; -- guest
+    END IF;
+
+    RETURN NEW;
+  END IF;
+
+  RETURN NULL;
+END;
+$$ LANGUAGE plpgsql
+   SECURITY DEFINER
+   SET search_path = db, kernel, pg_temp;
+
+CREATE OR REPLACE FUNCTION db.ft_profile_before()
+RETURNS trigger AS $$
+BEGIN
+  IF NEW.locale IS NULL THEN
+    SELECT id INTO NEW.locale FROM db.locale WHERE code = 'ru';
+  END IF;
+
+  IF NOT IsMemberArea(NEW.area, NEW.userid) THEN
+    SELECT '00000000-0000-4003-a000-000000000002' INTO NEW.area; -- guest
+  END IF;
+
+  IF NOT IsMemberInterface(NEW.interface, NEW.userid) THEN
+    SELECT '00000000-0000-4004-a000-000000000003' INTO NEW.interface; -- guest
+  END IF;
+
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql
+   SECURITY DEFINER
+   SET search_path = kernel, pg_temp;
