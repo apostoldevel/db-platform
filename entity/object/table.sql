6c4fa6bccd9a28fa1a8c2fd2b8a27ac421a21783
Преподобный Ален <alienufo@inbox.ru>
2021-05-26 18:47:44 +0300

    Updated: Object AOU.

--- entity/object/table.sql
+++ entity/object/table.sql
@@ -142,24 +142,29 @@ CREATE TRIGGER t_object_before_insert
 CREATE OR REPLACE FUNCTION db.ft_object_after_insert()
 RETURNS trigger AS $$
 DECLARE
---  uUserId	uuid;
+  uUserId    uuid;
+  vEntity   text;
 BEGIN
   INSERT INTO db.aom SELECT NEW.id;
   INSERT INTO db.aou (object, userid, deny, allow) SELECT NEW.id, userid, SubString(deny FROM 3 FOR 3), SubString(allow FROM 3 FOR 3) FROM db.acu WHERE class = NEW.class;
 
   INSERT INTO db.aou SELECT NEW.id, NEW.owner, B'000', B'111'
 	ON CONFLICT (object, userid) DO UPDATE SET deny = B'000', allow = B'111';
-/*
-  IF NEW.parent IS NOT NULL THEN
-    SELECT owner INTO uUserId FROM db.object WHERE id = NEW.parent;
-    IF NEW.owner <> uUserId THEN
-	  UPDATE db.aou SET allow = allow | B'100' WHERE object = NEW.id AND userid = uUserId;
-	  IF NOT FOUND THEN
-		INSERT INTO db.aou SELECT NEW.id, uUserId, B'000', B'100';
+
+  SELECT code INTO vEntity FROM db.entity WHERE id = NEW.entity;
+
+  IF vEntity = 'message' THEN
+	IF NEW.parent IS NOT NULL THEN
+	  SELECT owner INTO uUserId FROM db.object WHERE id = NEW.parent;
+	  IF NEW.owner <> uUserId THEN
+		UPDATE db.aou SET allow = allow | B'100' WHERE object = NEW.id AND userid = uUserId;
+		IF NOT FOUND THEN
+		  INSERT INTO db.aou SELECT NEW.id, uUserId, B'000', B'100';
+		END IF;
 	  END IF;
 	END IF;
   END IF;
-*/
+
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql
@@ -189,6 +194,7 @@ BEGIN
   END IF;
 
   IF NOT CheckObjectAccess(NEW.id, B'010') THEN
+    PERFORM WriteToEventLog('D', 9999, 'ft_object_before_update', row_to_json(NEW)::text, NEW.id);
     PERFORM AccessDenied();
   END IF;
 
