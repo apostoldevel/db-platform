4297d0a922410a0f4ffe33d610c19ae2697eb01d
Преподобный Ален <alienufo@inbox.ru>
2022-01-21 14:30:14 +0300

    Patch #27.

--- patch/P00000027.sql
+++ patch/P00000027.sql
@@ -11,3 +11,20 @@ ALTER TABLE db.object_file
   ALTER COLUMN owner SET NOT NULL;
 
 COMMENT ON COLUMN db.object_file.owner IS 'Владелец (пользователь)';
+
+CREATE OR REPLACE FUNCTION db.ft_object_file_insert()
+RETURNS trigger AS $$
+BEGIN
+  IF NEW.owner IS NULL THEN
+    NEW.owner := current_userid();
+  END IF;
+
+  IF NULLIF(NEW.file_path, '') IS NULL THEN
+    NEW.file_path := '~/';
+  END IF;
+
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql
+   SECURITY DEFINER
+   SET search_path = kernel, pg_temp;
