385b9bde1677cfd4c68ddf1cb6465c6dae9ec9c8
Преподобный Ален <alienufo@inbox.ru>
2022-04-18 14:50:49 +0300

    Updated Search.

--- entity/object/search.sql
+++ entity/object/search.sql
@@ -12,13 +12,13 @@ AS $$
   SELECT o.object
     FROM db.object_text o INNER JOIN access a ON o.object = a.object
    WHERE o.locale = '00000000-0000-4001-a000-000000000001'
-     AND (o.label ILIKE '%' || pText || '%'
-      OR o.text ILIKE '%' || pText || '%'
+     AND (o.label ILIKE pText || '%'
+      OR o.text ILIKE pText || '%'
       OR o.searchable_en @@ websearch_to_tsquery('english', pText))
    UNION
   SELECT r.object
     FROM db.reference r INNER JOIN access a ON r.object = a.object
-   WHERE r.code ILIKE '%' || pText || '%'
+   WHERE r.code LIKE pText || '%'
   ) SELECT o.* FROM api.object o INNER JOIN search s ON o.id = s.object;
 $$ LANGUAGE SQL
    SECURITY DEFINER
@@ -36,13 +36,13 @@ AS $$
   SELECT o.object
     FROM db.object_text o INNER JOIN access a ON o.object = a.object
    WHERE o.locale = '00000000-0000-4001-a000-000000000002'
-     AND (o.label ILIKE '%' || pText || '%'
-      OR o.text ILIKE '%' || pText || '%'
+     AND (o.label ILIKE pText || '%'
+      OR o.text ILIKE pText || '%'
       OR o.searchable_ru @@ websearch_to_tsquery('russian', pText))
    UNION
   SELECT r.object
     FROM db.reference r INNER JOIN access a ON r.object = a.object
-   WHERE r.code ILIKE '%' || pText || '%'
+   WHERE r.code LIKE pText || '%'
   ) SELECT o.* FROM api.object o INNER JOIN search s ON o.id = s.object;
 $$ LANGUAGE SQL
    SECURITY DEFINER
