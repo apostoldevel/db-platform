1245a71383d01868e991201d9a41d331ec9a04ca
Преподобный Ален <alienufo@inbox.ru>
2022-05-27 13:13:44 +0300

    Committing updates.

--- reports/routine.sql
+++ reports/routine.sql
@@ -1,4 +1,8 @@
 --------------------------------------------------------------------------------
+-- REPORT FORM -----------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
 -- rfc_identifier_form ---------------------------------------------------------
 --------------------------------------------------------------------------------
 /**
@@ -26,6 +30,72 @@ BEGIN
   END LOOP;
 
   RETURN json_build_object('form', pForm, 'fields', jsonb_build_array(jsonb_build_object('type', 'string', 'format', 'uuid', 'key', 'identifier', 'label', label)));
+END;
+$$ LANGUAGE plpgsql
+  SECURITY DEFINER
+  SET search_path = kernel, pg_temp;
+
+--------------------------------------------------------------------------------
+-- rfc_import_file -------------------------------------------------------------
+--------------------------------------------------------------------------------
+/**
+ * Форма импорта файла
+ * @param {uuid} pForm - Идентификатор формы
+ * @param {jsonb} pParams - Параметры
+ * @return {SETOF json} - Записи в JSON
+ */
+CREATE OR REPLACE FUNCTION report.rfc_import_file (
+  pForm         uuid,
+  pParams       json default null
+) RETURNS       json
+AS $$
+DECLARE
+  l             record;
+  label         text;
+BEGIN
+  FOR l IN SELECT code FROM db.locale WHERE id = current_locale()
+  LOOP
+    IF l.code = 'ru' THEN
+	  label := 'Импортировать файл';
+	ELSE
+      label := 'Import file';
+	END IF;
+  END LOOP;
+
+  RETURN json_build_object('form', pForm, 'fields', jsonb_build_array(jsonb_build_object('type', 'file', 'format', 'JSON', 'key', 'files', 'label', label)));
+END;
+$$ LANGUAGE plpgsql
+  SECURITY DEFINER
+  SET search_path = kernel, pg_temp;
+
+--------------------------------------------------------------------------------
+-- rfc_import_files ------------------------------------------------------------
+--------------------------------------------------------------------------------
+/**
+ * Форма импорта файлов
+ * @param {uuid} pForm - Идентификатор формы
+ * @param {jsonb} pParams - Параметры
+ * @return {SETOF json} - Записи в JSON
+ */
+CREATE OR REPLACE FUNCTION report.rfc_import_files (
+  pForm         uuid,
+  pParams       json default null
+) RETURNS       json
+AS $$
+DECLARE
+  l             record;
+  label         text;
+BEGIN
+  FOR l IN SELECT code FROM db.locale WHERE id = current_locale()
+  LOOP
+    IF l.code = 'ru' THEN
+	  label := 'Импортировать файлы';
+	ELSE
+      label := 'Import files';
+	END IF;
+  END LOOP;
+
+  RETURN json_build_object('form', pForm, 'fields', jsonb_build_array(jsonb_build_object('type', 'file', 'format', 'JSON', 'key', 'files', 'multiple', true, 'label', label)));
 END;
 $$ LANGUAGE plpgsql
   SECURITY DEFINER
