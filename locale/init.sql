34228aedd36dd2788e596ce374bb53003a676e7e
Преподобный Ален <alienufo@inbox.ru>
2025-09-09 12:09:40 +0300

    Committing updates.

--- locale/init.sql
+++ locale/init.sql
@@ -2,6 +2,8 @@
 -- Initialization --------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-INSERT INTO db.locale (id, code, name, description) VALUES ('00000000-0000-4001-a000-000000000001', 'en', 'English', 'English - United States');
-INSERT INTO db.locale (id, code, name, description) VALUES ('00000000-0000-4001-a000-000000000002', 'ru', 'Русский', 'Русский язык');
-
+INSERT INTO db.locale (id, code, name, description) VALUES ('00000000-0000-4001-a000-000000000001', 'en', 'English',   'English language');
+INSERT INTO db.locale (id, code, name, description) VALUES ('00000000-0000-4001-a000-000000000002', 'ru', 'Русский',  'Русский язык');
+INSERT INTO db.locale (id, code, name, description) VALUES ('00000000-0000-4001-a000-000000000003', 'de', 'Deutsch',  'Deutsche Sprache');
+INSERT INTO db.locale (id, code, name, description) VALUES ('00000000-0000-4001-a000-000000000004', 'fr', 'Français', 'Langue française');
+INSERT INTO db.locale (id, code, name, description) VALUES ('00000000-0000-4001-a000-000000000005', 'it', 'Italiano', 'Lingua italiana');
